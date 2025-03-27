433affeab5457440c8b555204758f2160aadc5fd
Alb33rt <albert931023@gmail.com>
2025-03-26 20:21:25 -0400

    [0002] Complete prototype RANSAC node logic

--- ros2/src/mfe_perception/lidar_cone_detector/include/lidar_cone_detector/ground_plane_removal.hpp
+++ ros2/src/mfe_perception/lidar_cone_detector/include/lidar_cone_detector/ground_plane_removal.hpp
@@ -4,6 +4,9 @@
 #include <vector>
 
 // dependencies that require ament_cmake to imports
+#include "rclcpp/qos.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 #include "Eigen/Dense"
 #include "pcl/io/pcd_io.h"
 #include "pcl/point_types.h"
@@ -14,22 +17,36 @@
 #include "pcl/visualization/pcl_visualizer.h"
 #include "pcl/common/angles.h"
 
+#include "pcl_conversions/pcl_conversions.h"    // Interface for ROS and PCL
+
 namespace lidar_cone_detector {
     class GroundPlaneRemovalNode : public rclcpp::Node 
     {
         public:
             GroundPlaneRemovalNode(const rclcpp::NodeOptions &options);
 
+            // public params of the node
+            bool run_visualization;
+
         private:
             // sharedptr used for thread saafety and automatic memory management
-            rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_sub;
-            rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_ground_pub;
-            rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_pub;
+            rclcpp::Subscription<sensor_msgs::msg::PointCloud2> point_cloud_sub;
+            rclcpp::Subscription<sensor_msgs::msg::PointCloud2> point_cloud_ground_pub;
+            rclcpp::Subscription<sensor_msgs::msg::PointCloud2> point_cloud_cones_pub;
 
-            void remove_ground_plane(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+            void remove_ground_plane_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
             void visualize(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pcd, const Eigen::Vector3f &vehicle_position);
 
             // params of the node
+            // TODO: parameterize (generalize) the algorithm variables to ROS parameters
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr acc_cloud;  // Accumulative cloud the algorithm operates on
+
+            pcl::VoxelGrid<pcl::PointXYZ> sor;
+            pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor_outlier;
+            pcl::SACSegmentation<pcl::PointXYZ> seg;
+            pcl::ExtractIndices<pcl::PointXYZ> extract;
+
     }
 
     std::vector<Eigen::Vector3f> load_bin_pointcloud(const std::string &bin_file);
--- ros2/src/mfe_perception/lidar_cone_detector/src/ground_plane_removal.cpp
+++ ros2/src/mfe_perception/lidar_cone_detector/src/ground_plane_removal.cpp
@@ -8,52 +8,32 @@ namespace lidar_cone_detector {
         public:
             GroundPlaneRemovalNode() : Node("ground_plane_removal_node")
             {
-                // TODO: Rename topic names to better fit
+                this->declare_parameter("run_visualization", false);
+                this->get_parameter("run_visualization", this->run_visualization);
+
+                // Subscribes to the general point cloud and publishes ground data and the rest in two separate streams
                 point_cloud_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-                    "pcl_points", rclcpp::SensorDataQoS(),
-                    std::bind(&GroundPlaneRemovalNode::remove_ground_plane, this)
+                    "lidar/pcl/raw", rclcpp::SensorDataQoS(),
+                    std::bind(&GroundPlaneRemovalNode::remove_ground_plane_callback, this, std::placeholders::_1)
                 );
                 point_cloud_ground_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-                    "lidar/ground_pcl", rclcpp::SensorDataQoS()
+                    "lidar/pcl/ground", rclcpp::SensorDataQoS()
                 );
-                point_cloud_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-                    "lidar/pcl", rclcpp::SensorDataQoS()
+                point_cloud_cones_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+                    "lidar/pcl/cones", rclcpp::SensorDataQoS()
                 );
 
-                // define appropriate inner params to use
+                // params defined in header file
             }
-z
+
         private:
-            rclcpp::TimerBase::SharedPtr timer_;
+            rclcpp::TimerBase::SharedPtr timer_;    // Used to measure performance metrics
 
             // copy of the main method from 02/2025 demo ransac
-            void remove_ground_plane(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+            void remove_ground_plane_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
             {
-                // Dataset path
-                std::string path_to_dataset = "./cfs_vargarda8/";
-
-                // Load JSON metadata
-                std::ifstream json_file(path_to_dataset + "metadata.json");
-                Json::Value data;
-                json_file >> data;
-
-                // Load point cloud from .bin file
-                int i = 0;
-                std::string bin_file = path_to_dataset + data["data"][i]["pointcloud"]["file"].asString();
-                std::cout << "Loading point cloud from: " << bin_file << std::endl;
-                auto points = load_bin_pointcloud(bin_file);
-
-                // Convert to PCL PointCloud
                 pcl::PointCloud<pcl::PointXYZ>::Ptr pcd(new pcl::PointCloud<pcl::PointXYZ>);
-                for (const auto &point : points)
-                {
-                    pcd->points.push_back(pcl::PointXYZ(point.x(), point.y(), point.z()));
-                }
-                std::cout << "Loaded point cloud with " << pcd->points.size() << " points." << std::endl;
-
-                // Save the loaded point cloud
-                pcl::PCDWriter writer;
-                writer.writeBinary("start.pcd", *pcd);
+                pcl::fromROSMsg(*msg, *pcd);
 
                 // TODO: filter remove all points beyond a certain distance from the origin
 
@@ -64,7 +44,6 @@ z
                 // can change how many points are in each box with setMinimumPointsNumberPerVoxel
                 pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled_pcd(new pcl::PointCloud<pcl::PointXYZ>);
                 sor.filter(*downsampled_pcd);
-                writer.writeBinary("downsample.pcd", *downsampled_pcd);
 
                 // Remove statistical outliers
                 pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor_outlier;
@@ -73,29 +52,35 @@ z
                 sor_outlier.setStddevMulThresh(1.0); // threshold of std devs away from mean that will be marked as outlier
                 pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_pcd(new pcl::PointCloud<pcl::PointXYZ>);
                 sor_outlier.filter(*filtered_pcd);
-                writer.writeBinary("filtered.pcd", *filtered_pcd);
 
                 // Plane segmentation (RANSAC)
                 pcl::SACSegmentation<pcl::PointXYZ> seg;
-                pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-                pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-
                 seg.setOptimizeCoefficients(true);
                 seg.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
                 seg.setMethodType(pcl::SAC_RANSAC);
-
                 seg.setAxis(Eigen::Vector3f(0, 0, 1)); // Forces a horizontal plane
-                // seg.setEpsAngle(pcl::deg2rad(10.0));   // Allow slight angle variation
-
                 seg.setMaxIterations(1000);
                 seg.setDistanceThreshold(0.05);
                 seg.setInputCloud(filtered_pcd);
+
+                pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+                pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+                
                 seg.segment(*inliers, *coefficients);
 
-                std::cout << "Plane equation: " << coefficients->values[0] << "x + "
-                        << coefficients->values[1] << "y + "
-                        << coefficients->values[2] << "z + "
-                        << coefficients->values[3] << " = 0" << std::endl;
+                // Logging coefficient data
+                if (coefficients->values.size() >= 4) {
+                    RCLCPP_INFO(
+                        this->get_logger(),
+                        "RANSAC plane coefficients: [a=%f, b=%f, c=%f, d=%f]",
+                        coefficients->values[0],
+                        coefficients->values[1],
+                        coefficients->values[2],
+                        coefficients->values[3]
+                    );  // macro for logger
+                } else {
+                    RCLCPP_WARN(this->get_logger(), "Not enough coefficients returned by the segmenter.");
+                }
 
                 // Extract inliers and outliers
                 pcl::ExtractIndices<pcl::PointXYZ> extract;
@@ -106,22 +91,41 @@ z
                 extract.setIndices(inliers);
                 extract.setNegative(false);
                 extract.filter(*outlier_cloud);
-
                 extract.setNegative(true);
                 extract.filter(*inlier_cloud);
 
-                writer.writeBinary("remove_ground.pcd", *inlier_cloud);
-                writer.writeBinary("ground.pcd", *outlier_cloud);
+                // Output processed data and publish to topics
+                sensor_msgs::msg::PointCloud2 cones_output;
+                sensor_msgs::msg::PointCloud2 ground_output;
 
-                // Visualize the point cloud using PCL visualization
-                Eigen::Vector3f vehicle_position(data["data"][i]["odom"]["x"].asFloat(),
-                                                data["data"][i]["odom"]["y"].asFloat(),
-                                                data["data"][i]["odom"]["z"].asFloat());
+                pcl::toROSMsg(*inlier_cloud, cones_output);
+                pcl::toROSMsg(*outlier_cloud, ground_output);
 
-                this->visualize(inlier_cloud, vehicle_position);
+                cones_output.header = msg->header;
+                ground_output.header = msg->header;
+
+                point_cloud_cones_pub->publish(cones_output);
+                point_cloud_ground_pub->publish(ground_output);
+
+                // Perform visualization depending on if set in ROS params
+                // WARNING: this does not work yet
+                if (this->run_visualization)
+                {
+                    // Visualize the point cloud using PCL visualization
+                    // Eigen::Vector3f vehicle_position(data["data"][i]["odom"]["x"].asFloat(),
+                    // data["data"][i]["odom"]["y"].asFloat(),
+                    // data["data"][i]["odom"]["z"].asFloat());
+                    // this->visualize(inlier_cloud, vehicle_position);
+                }
             }
 
-            // execute visualization usin RViz2
+            /*
+                Performs visualization of the RANSAC algorithm using RViz2. 
+
+                Note: this function should only execute when ROS2 Parameter is setup properly.
+                @param pcd pcl::PointerCloud<pcl::PointXYZ> The point cloud
+                @param vehicle_position Eigen::Vector3f The 3D coordinate of the vehicle from SLAM
+            */
             void visualize(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pcd, const Eigen::Vector3f &vehicle_position)
             {
                 pcl::visualization::PCLVisualizer viewer("Point Cloud Viewer");
@@ -142,9 +146,16 @@ z
                     viewer.spinOnce(100);
                 }
             }
-    }
+    };
 
-    // Function to load the LiDAR point cloud from a binary file (KITTI format)
+    /*
+        Loads a LiDAR point cloud from a binary file using the KITTI format.
+
+        (Deprecation warning) Currently not in use.
+
+        @param bin_file pointer to the binary file
+        @return point cloud as a std::vector of Eigen::Vector3f
+    */
     std::vector<Eigen::Vector3f> load_bin_pointcloud(const std::string &bin_file)
     {
         std::ifstream file(bin_file, std::ios::binary);
